5fa1a8abecdf832c1d7e89772f5c135edf2ccc26
6e0r9e <1@6e0r9e.com>
2013-12-04 16:18:27 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppointmentApi::Application.config.secret_key_base = '1df6698df1f593c905767c39494878074c2593869a34e82b8af00d2bd47809737a86ff021918559ea5c0fdceb69856af527529351014f19574d0db523a2913f0'
